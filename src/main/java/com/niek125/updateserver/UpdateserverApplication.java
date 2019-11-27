8afb8b607d39dce4200390b65b11b9e72b300682
Niek125 <niek.sleddens@gmail.com>
2019-11-27 22:08:41 +0100

    initial commit

---
+++ src/main/java/com/niek125/updateserver/UpdateserverApplication.java
@@ -0,0 +1,13 @@
+package com.niek125.updateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpdateserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niek125/updateserver/UpdateserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niek125.updateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
